c20e15bcfe1df5926c6cb9b2aba682545fcb80e5
Gordon Chiam <gordon.chiam@grasshopperasia.com>
2014-02-27 11:49:52 +0800

    add git alias st

--- bin/setup_git.sh
+++ bin/setup_git.sh
@@ -2,4 +2,5 @@
 
 # http://fredkschott.com/post/2014/02/git-log-is-so-2005/
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.st "status"
 
