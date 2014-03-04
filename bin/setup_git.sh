939eb4daf5cea0c21e335f093d4c23d73aa1f947
Gordon Chiam <gordon.chiam@grasshopperasia.com>
2014-03-04 15:54:16 +0800

    git: use vimdiff for git difftool

--- bin/setup_git.sh
+++ bin/setup_git.sh
@@ -2,5 +2,8 @@
 
 # http://fredkschott.com/post/2014/02/git-log-is-so-2005/
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit"
+
 git config --global alias.st "status"
 
+git config --global diff.tool "vimdiff"
+git config --global difftool.prompt "no"
