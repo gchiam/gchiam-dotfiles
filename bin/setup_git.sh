332dbbc435de20366ff51832ab3e1a8ad41d2eb1
gchiam <gordon.chiam@grasshopperasia.com>
2014-03-12 14:33:43 +0800

    git: add a few more git aliases

--- bin/setup_git.sh
+++ bin/setup_git.sh
@@ -3,7 +3,13 @@
 # http://fredkschott.com/post/2014/02/git-log-is-so-2005/
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit"
 
-git config --global alias.st "status"
+git config --global alias.br 'branch'
+git config --global alias.co 'checkout'
+git config --global alias.ci 'commit'
+git config --global alias.st 'status'
+git config --global alias.unstage 'reset HEAD --'
 
-git config --global diff.tool "vimdiff"
-git config --global difftool.prompt "no"
+git config --global alias.last 'log -1 HEAD'
+
+git config --global diff.tool 'vimdiff'
+git config --global difftool.prompt 'no'
