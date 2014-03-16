7410f75465ec281583c13fc058b159aa4a58a5a0
gchiam <gordon.chiam@gmail.com>
2014-03-16 21:25:04 +0800

    fixed bin/setup_fonts.sh

--- bin/setup_fonts.sh
+++ bin/setup_fonts.sh
@@ -2,11 +2,13 @@
 
 fonts_dir="${HOME}/.fonts"
 base_dir="${HOME}/projects/gchiam-dotfiles"
-powerline_fonts_dir="${base_dir}/external/powerline-fonts/"
+powerline_fonts_dir="${base_dir}/external/powerline-fonts"
 
 mkdir -p ${fonts_dir}
 
 for f in `ls  -1 ${powerline_fonts_dir}`
 do
-    test -d $f && ln -snvf ${powerline_fonts_dir}/$f ${fonts_dir}/$f
+    source="${powerline_fonts_dir}/$f"
+    target="${fonts_dir}/$f"
+    test -d $source && ln -snvf $source $target
 done
