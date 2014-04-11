dc7fc6db33e0d0f1047e87705941af0da5dcde72
gchiam <gordon.chiam@grasshopperasia.com>
2014-04-11 14:31:30 +0800

    add a help script to refresh submodules

---
+++ bin/pull_submodules.sh
@@ -0,0 +1,22 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: pull_submodules.sh
+# 
+#         USAGE: ./pull_submodules.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Gordon Chiam NAME (gordon.chiam@gmail.com), 
+#  ORGANIZATION: 
+#       CREATED: 04/11/2014 14:29
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+git submodule foreach git pull origin master
