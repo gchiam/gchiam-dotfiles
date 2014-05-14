0fd3b784fc1c824b973f493a4a031d208dce77e9
Gordon Chiam <gordon.chiam@grasshopperasia.com>
2014-05-14 12:51:20 +0800

    more git aliases

--- bin/setup_git.sh
+++ bin/setup_git.sh
@@ -5,7 +5,9 @@ git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Crese
 
 git config --global alias.br 'branch'
 git config --global alias.co 'checkout'
+git config --global alias.master 'checkout master'
 git config --global alias.ci 'commit'
+git config --global alias.pl 'pull'
 git config --global alias.st 'status'
 git config --global alias.unstage 'reset HEAD --'
 
