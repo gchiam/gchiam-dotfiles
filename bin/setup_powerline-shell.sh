68e1da97e1df1b603c9274eafe2e8b56661f0752
Gordon Chiam <gordon.chiam@grasshopperasia.com>
2014-10-03 08:42:01 +0800

    removed deprecated script

--- bin/setup_powerline-shell.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pushd external/powerline-shell/
-python install.py
-sed -i "s/THEME = '.*'/THEME = 'solarized-dark'/" config.py
-python install.py
-popd
-ln -snvf external/powerline-shell/powerline-shell.py powerline-shell.py
