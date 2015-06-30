7a468f4ad733fbf7ce6e6e77be331398bef14efa
Gordon Chiam <gordon.chiam@grasshopperasia.com>
2015-06-30 08:15:38 +0800

    add pylintrc

--- bin/setup.sh
+++ bin/setup.sh
@@ -11,6 +11,7 @@ ln -snvf $DOTFILES_DIR/.bash_aliases $HOME/
 ln -snvf $DOTFILES_DIR/.bash_exports $HOME/
 ln -snvf $DOTFILES_DIR/.bash_extra $HOME/
 ln -snvf $DOTFILES_DIR/.zshrc $HOME/
+ln -snvf $DOTFILES_DIR/.pylintrc $HOME/
 ln -snvf $DOTFILES_DIR/.oh-my-zsh $HOME/
 ln -snvf $DOTFILES_DIR/.django_bash_completion $HOME/
 ln -snvf $DOTFILES_DIR/.tmux.conf $HOME/
