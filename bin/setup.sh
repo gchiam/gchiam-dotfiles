e5071bfecbba9160d253895166af6a2d6464a9ba
Gordon Chiam <gordon.chiam@grasshopperasia.com>
2015-10-05 09:57:33 +0800

    submodule: add gruvbox-contrib

--- bin/setup.sh
+++ bin/setup.sh
@@ -24,6 +24,7 @@ ln -snvf $DOTFILES_DIR/.vimrc $HOME/
 ln -snvf $DOTFILES_DIR/.nvim $HOME/
 ln -snvf $DOTFILES_DIR/.nvimrc $HOME/
 ln -snvf $DOTFILES_DIR/.fonts.conf $HOME/
+ln -snvf $DOTFILES_DIR/.Xresources $HOME/
 ln -snvf $DOTFILES_DIR/external/powerline/powerline/bindings/bash/powerline.sh $HOME/.powerline.sh
 ln -snvf $DOTFILES_DIR/external/powerline/powerline/bindings/zsh/powerline.zsh $HOME/.powerline.zsh
 ln -snvf $DOTFILES_DIR/external/powerline/scripts/powerline $HOME/bin/
