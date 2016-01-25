aa22095d3f25d7137e05a52b8902a62c080cd414
Gordon Chiam <gordon.chiam@gmail.com>
2016-01-25 12:27:56 +0800

    Update commit_vim_plugged_modules.sh

--- bin/deploy/commit_vim_plugged_modules.sh
+++ bin/deploy/commit_vim_plugged_modules.sh
@@ -4,4 +4,4 @@ rm -f .vim/plugged/vim-expand-region/doc/tags
 rm -f .vim/plugged/xterm-color-table.vim/doc/tags
 
 git add .vim/plugged
-git commit -m "vim: update vim-plugged modules"
+git commit -m "vim: Update vim-plugged modules"
