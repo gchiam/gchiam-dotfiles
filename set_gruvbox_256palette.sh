f38dba29d232cc1ae0447341fbdfa59b5d9718f1
Gordon Chiam <gordon.chiam@gmail.com>
2016-04-22 13:09:40 +0800

    Fix path

--- set_gruvbox_256palette.sh
+++ set_gruvbox_256palette.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  source .vim/plugged/gruvbox/gruvbox_256palette_osx.sh
+  source $DOTFILES_DIR/.vim/plugged/gruvbox/gruvbox_256palette_osx.sh
 else
-  source .vim/plugged/gruvbox/gruvbox_256palette.sh
+  source $DOTFILES_DIR/.vim/plugged/gruvbox/gruvbox_256palette.sh
 fi
