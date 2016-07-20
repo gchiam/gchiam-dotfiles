e3b2f497389e1c2ae0dc77f7ef14b04565d2c7ca
Gordon Chiam <gordon.chiam@gmail.com>
2016-07-20 11:00:54 +0800

    bash: Improve neovim virtaulenv bash scripts

--- bin/update_neovim_virtualenv.sh
+++ bin/update_neovim_virtualenv.sh
@@ -1,9 +1,16 @@
-workon neovim-py2
-pip install -U pip
-pip install -U -r pip-requirements/neovim.txt
-deactivate
+# vim: set filetype=sh:
 
-workon neovim-py3
-pip install -U pip
-pip install -U -r pip-requirements/neovim.txt
-deactivate
+
+. ~/.bash_virtualenvwrapper
+
+update_env() {
+    venv_name="$1"
+    venv_dir="${WORKON_HOME}/${venv_name}"
+
+    PIP="${venv_dir}/bin/pip"
+    $PIP install -U pip
+    $PIP install -U -r pip-requirements/neovim.txt
+}
+
+update_env neovim-py2
+update_env neovim-py3
