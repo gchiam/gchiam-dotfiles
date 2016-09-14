99154dbefa0956babb26679859bb71f7a7b68797
Gordon Chiam <gordon.chiam@gmail.com>
2016-09-14 11:33:30 +0800

    Fix create_neovim_virtualenv.sh

--- bin/create_neovim_virtualenv.sh
+++ bin/create_neovim_virtualenv.sh
@@ -4,16 +4,17 @@
 . ~/.bash_virtualenvwrapper
 
 make_venv() {
-    venv_name="$1"
+    PYTHON="$1"
+    venv_name="$2"
     venv_dir="${WORKON_HOME}/${venv_name}"
 
     test -e ${venv_dir} && rm -rf ${venv_dir}
 
-    mkvirtualenv -p `which python2` ${venv_name}
+    mkvirtualenv -p $PYTHON ${venv_name}
     PIP="${venv_dir}/bin/pip"
     $PIP install -U pip
     $PIP install -U -r pip-requirements/neovim.txt
 }
 
-make_venv neovim-py2
-make_venv neovim-py3
+make_venv `which python2` neovim-py2
+make_venv `which python3` neovim-py3
