7ece3b5cc9b9c5a531ab69e31896de3e503f22da
Gordon Chiam <gordon.chiam@gmail.com>
2016-11-18 21:43:55 +0800

    Update command to install homebrew

--- bin/setup-osx.sh
+++ bin/setup-osx.sh
@@ -5,7 +5,7 @@
 xcode-select --install
 
 # install homebrew
-ruby -e “$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)”
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor
 brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
