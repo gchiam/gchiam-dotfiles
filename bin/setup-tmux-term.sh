ff53ed0b6164eca12ee4e91f44eb155fe92ea4ae
Gordon Chiam <gordon.chiam@gmail.com>
2017-05-24 07:36:05 +0000

    tmux: Attempt to term issue

---
+++ bin/setup-tmux-term.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+cat <<EOF|tic -x -
+tmux|tmux terminal multiplexer,
+  ritm=\E[23m, rmso=\E[27m, sitm=\E[3m, smso=\E[7m, Ms@,
+  use=xterm+tmux, use=screen,
+
+tmux-256color|tmux with 256 colors,
+  use=xterm+256setaf, use=tmux,
+EOF
