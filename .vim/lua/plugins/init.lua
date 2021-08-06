bb22880070c9c32f0cd6f7bd0b7d872548d4857f
Gordon Chiam <gchiam@zendesk.com>
2021-08-07 03:46:45 +0800

    vim: Setup lsp and autocomplete

--- .vim/lua/plugins/init.lua
+++ .vim/lua/plugins/init.lua
@@ -1,5 +1,8 @@
 -- vim: set tabstop=2 softtabstop=2 shiftwidth=2 expandtab:
 
+require "plugins.nvim-lspconfig"
+require "plugins.nvim-compe"
+
 --
 -- color scheme
 require "plugins.nord"
@@ -8,4 +11,3 @@ require "plugins.nord"
 -- UI
 require "plugins.nvim-web-devicons"
 require "plugins.lualine"
-
