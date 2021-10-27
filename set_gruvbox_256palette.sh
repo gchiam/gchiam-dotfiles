69150db6fac76fd48a51cd3778f0666c6b106da3
Gordon Chiam <gordon.chiam@gmail.com>
2021-10-28 02:08:45 +0800

    Remove set_gruvbox_256palette.sh

--- set_gruvbox_256palette.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  source $DOTFILES_DIR/.vim/repos/github.com/morhetz/gruvbox/gruvbox_256palette_osx.sh
-else
-  source $DOTFILES_DIR/.vim/repos/github.com/morhetz/gruvbox/gruvbox_256palette.sh
-fi
