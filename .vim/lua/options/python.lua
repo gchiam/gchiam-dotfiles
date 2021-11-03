7d2f9d8abf553c17186d4616d14c0988f9f94444
Gordon Chiam <gchiam@zendesk.com>
2021-11-03 11:13:57 +0800

    neovim: Fix python setup

--- .vim/lua/options/python.lua
+++ .vim/lua/options/python.lua
@@ -2,7 +2,6 @@ vim.g.python_highlight_all = 1
 
 cmd([[
   if has("unix")
-    let s:uname = system("uname")
     let g:python3_host_prog=$HOME . '/.pyenv/versions/neovim-py3/bin/python'
     let g:python2_host_prog=$HOME . '/.pyenv/versions/neovim-py2/bin/python'
     let g:python_host_prog=$HOME . '/.pyenv/versions/neovim-py2/bin/python'
