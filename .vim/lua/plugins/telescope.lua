e22964beb11a5375e8fe35eb314de3b1ad58e48f
Gordon Chiam <gordon.chiam@gmail.com>
2022-02-10 02:43:11 +0800

    vim: Setup telescope-fzf-native plugin

--- .vim/lua/plugins/telescope.lua
+++ .vim/lua/plugins/telescope.lua
@@ -1,6 +1,20 @@
 -- https://github.com/nvim-telescope/telescope.nvim-telescope
 
 use "nvim-telescope/telescope.nvim"
+use "nvim-telescope/telescope-fzf-native.nvim"
+
+require('telescope').setup {
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    }
+  }
+}
+require('telescope').load_extension('fzf')
 
 nmap("<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<CR>")
 nmap("<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<CR>")
