5a2a1826220e27a926aab190d657c59b9f0b0c14
Gordon Chiam <gordon.chiam@gmail.com>
2024-08-22 11:48:36 +0800

    wezterm: Translucent window effect

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -12,6 +12,10 @@ config.harfbuzz_features = { 'calt', 'clig', 'liga', 'zero' }
 config.line_height = 1.1
 config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 
+-- Translucent window effect
+config.window_background_opacity = 0.7
+config.macos_window_background_blur = 30
+
 -- and finally, return the configuration to wezterm
 return config
 
