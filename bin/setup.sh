3c7f3f52491e11a9cfcfec504925ab32f2509f98
Gordon Chiam <gordon.chiam@gmail.com>
2024-08-26 02:07:50 +0800

    Remove .Xresources file

--- bin/setup.sh
+++ bin/setup.sh
@@ -16,7 +16,5 @@ for d in $(ls -1 ${stow_dir}); do
   stow --dir=${stow_dir} --target=$HOME --restow $d
 done
 
-ln -snvf $DOTFILES_DIR/.Xresources $HOME/
-
 # generate ~/fleet.properties
 echo "fleet.config.path=${HOME}/.config/JetBrains/Fleet/" > ~/fleet.properties
