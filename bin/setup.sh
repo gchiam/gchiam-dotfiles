29876821e529464db024e4a183c2e8b4578fadf4
Gordon Chiam <gordon.chiam@gmail.com>
2024-08-26 01:11:08 +0800

    Use stow to symlink nvim config

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 DOTFILES_DIR="$HOME/dotfiles"
-
 ln -snvf $HOME/projects/gchiam-dotfiles $DOTFILES_DIR
+
+# Run stow
+stow_dir=${DOTFILES_DIR}/stow
+if ! command -v stow &> /dev/null
+then
+    echo "stow could not be found. Will not brew install stow."
+    brew install stow
+fi
+
+for d in $(ls -1 ${stow_dir}); do
+  echo "stowing $d..."
+  stow --dir=${stow_dir} --target=$HOME --restow $d
+done
 
 ln -snvf $DOTFILES_DIR/.bashrc $HOME/
 ln -snvf $DOTFILES_DIR/.bash_profile $HOME/
