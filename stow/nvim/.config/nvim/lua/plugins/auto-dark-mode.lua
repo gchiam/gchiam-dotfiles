6e98d9814da752042e9059ba3bbd9a36163e999f
Gordon Chiam <gordon.chiam@gmail.com>
2024-09-05 01:03:37 +0800

    lazyvim: Install f-person/auto-dark-mode.nvim plugin

---
+++ stow/nvim/.config/nvim/lua/plugins/auto-dark-mode.lua
@@ -0,0 +1,13 @@
+return {
+  "f-person/auto-dark-mode.nvim",
+  opts = {
+    update_interval = 1000,
+    set_dark_mode = function()
+      vim.api.nvim_set_option_value("background", "dark", {})
+    end,
+    set_light_mode = function()
+      vim.api.nvim_set_option_value("background", "light", {})
+    end,
+  },
+}
+
