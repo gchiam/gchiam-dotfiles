4a430eb2767739a30ca11736a9a646f32436ceaf
Gordon Chiam <gordon.chiam@gmail.com>
2025-10-18 02:47:02 +0800

    fix(neotest): simplify configuration and resolve plugin spec error
    Resolves #48

--- stow/nvim/.config/nvim/lua/plugins/neotest.lua
+++ stow/nvim/.config/nvim/lua/plugins/neotest.lua
@@ -5,19 +5,9 @@ return {
     "weilbith/neotest-gradle",
   },
   opts = {
-    -- Can be a list of adapters like what neotest expects,
-    -- or a list of adapter names,
-    -- or a table of adapter names, mapped to adapter configs.
-    -- The adapter will then be automatically loaded with the config.
     adapters = {
-      'neotest-gradle',
+      "neotest-gradle",
     },
-    -- Example for loading neotest-go with a custom config
-    -- adapters = {
-    --   ["neotest-go"] = {
-    --     args = { "-tags=integration" },
-    --   },
-    -- },
     status = { virtual_text = true },
     output = { open_on_run = true },
     quickfix = {
@@ -31,74 +21,35 @@ return {
     },
   },
   config = function(_, opts)
-    local neotest_ns = vim.api.nvim_create_namespace("neotest")
-    vim.diagnostic.config({
-      virtual_text = {
-        format = function(diagnostic)
-          -- Replace newline and tab characters with space for more compact diagnostics
-          local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
-          return message
-        end,
-      },
-    }, neotest_ns)
-
     if LazyVim.has("trouble.nvim") then
-      opts.consumers = opts.consumers or {}
-      -- Refresh and auto close trouble after running tests
-      ---@type neotest.Consumer
-      opts.consumers.trouble = function(client)
-        client.listeners.results = function(adapter_id, results, partial)
-          if partial then
-            return
-          end
-          local tree = assert(client:get_position(nil, { adapter = adapter_id }))
-
-          local failed = 0
-          for pos_id, result in pairs(results) do
-            if result.status == "failed" and tree:get_key(pos_id) then
-              failed = failed + 1
+      opts.consumers = {
+        trouble = function(client)
+          client.listeners.results = function(adapter_id, results, partial)
+            if partial then
+              return
             end
-          end
-          vim.schedule(function()
-            local trouble = require("trouble")
-            if trouble.is_open() then
-              trouble.refresh()
-              if failed == 0 then
-                trouble.close()
+            local tree = assert(client:get_position(nil, { adapter = adapter_id }))
+
+            local failed = 0
+            for pos_id, result in pairs(results) do
+              if result.status == "failed" and tree:get_key(pos_id) then
+                failed = failed + 1
               end
             end
-          end)
-          return {}
-        end
-      end
-    end
-
-    if opts.adapters then
-      local adapters = {}
-      for name, config in pairs(opts.adapters or {}) do
-        if type(name) == "number" then
-          if type(config) == "string" then
-            config = require(config)
+            vim.schedule(function()
+              local trouble = require("trouble")
+              if trouble.is_open() then
+                trouble.refresh()
+                if failed == 0 then
+                  trouble.close()
+                end
+              end
+            end)
+            return {}
           end
-          adapters[#adapters + 1] = config
-        elseif config ~= false then
-          local adapter = require(name)
-          if type(config) == "table" and not vim.tbl_isempty(config) then
-            local meta = getmetatable(adapter)
-            if adapter.setup then
-              adapter.setup(config)
-            elseif meta and meta.__call then
-              adapter(config)
-            else
-              error("Adapter " .. name .. " does not support setup")
-            end
-          end
-          adapters[#adapters + 1] = adapter
-        end
-      end
-      opts.adapters = adapters
+        end,
+      }
     end
-
     require("neotest").setup(opts)
   end,
   -- stylua: ignore
